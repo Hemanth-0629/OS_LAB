eb139b71b72841dd5082552cfb63813f241a7723
hemanth <gurramhemanth0629@gmail.com>
2023-10-28 15:56:22 +0530

    update

--- LAB-11.c
+++ LAB-11.c
@@ -61,7 +61,7 @@ bool request_is_safe(int process, int request[NUM_RESOURCES]) {
                 }
             }
         }
-        if (!foun d) {
+        if (!found) {
             break;
         }
     }
